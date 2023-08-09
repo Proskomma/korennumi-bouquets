2e1a2692ed7ea30a09b814d354b6a230cbbebc54
Mark Howe <mvahowe@gmail.com>
2023-08-09 14:31:18 +0200

    First stab at JXL

---
+++ scripts/jxl2tsv/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const fse = require('fs-extra');
+
+const jxlJson = fse.readJsonSync(path.resolve(process.argv[2]));
+
+let n = 0;
+for (const sentence of jxlJson) {
+    n++;
+    const clauseRows = sentence.chunks.map(ch => '| ' + ch.source.map(s => s.content).join(' ') + ' | ' + ch.gloss + ' |').join('\\n');
+    const cvs = Array.from(new Set(sentence.chunks.map(ch => ch.source.map(s => s.cv)).reduce((a, b) => [...a, ...b], [])));
+    console.log(`${cvs[0]}\t${cvs[cvs.length - 1]}\t${n}\t${"| GRC | FRA |\\n| --: | :-- |\\n" + clauseRows}`);
+}
