4a28472f4d34be42dda03acba7ccb00167845eeb
Mark Howe <mvahowe@gmail.com>
2023-08-09 14:40:34 +0200

    Add bookname to JXL TSV

--- scripts/jxl2tsv/index.js
+++ scripts/jxl2tsv/index.js
@@ -8,5 +8,5 @@ for (const sentence of jxlJson) {
     n++;
     const clauseRows = sentence.chunks.map(ch => '| ' + ch.source.map(s => s.content).join(' ') + ' | ' + ch.gloss + ' |').join('\\n');
     const cvs = Array.from(new Set(sentence.chunks.map(ch => ch.source.map(s => s.cv)).reduce((a, b) => [...a, ...b], [])));
-    console.log(`${cvs[0]}\t${cvs[cvs.length - 1]}\t${n}\t${"| GRC | FRA |\\n| --: | :-- |\\n" + clauseRows}`);
+    console.log(`PHP ${cvs[0]}\tPHP ${cvs[cvs.length - 1]}\t${n}\t${"| GRC | FRA |\\n| --: | :-- |\\n" + clauseRows}`);
 }
